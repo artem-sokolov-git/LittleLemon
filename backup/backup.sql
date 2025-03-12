02b0f4dcf89ff100596aa0b5815b19245685b69c
artem-sokolov-git <artem.sokolov.dev@gmail.com>
2025-03-12 09:56:52 -0400

    add menu and booking models and refactoring structure of project

--- backup/backup.sql
+++ backup/backup.sql
@@ -84,7 +84,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +93,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can view log entry',1,'view_logentry'),(5,'Can add permission',2,'add_permission'),(6,'Can change permission',2,'change_permission'),(7,'Can delete permission',2,'delete_permission'),(8,'Can view permission',2,'view_permission'),(9,'Can add group',3,'add_group'),(10,'Can change group',3,'change_group'),(11,'Can delete group',3,'delete_group'),(12,'Can view group',3,'view_group'),(13,'Can add user',4,'add_user'),(14,'Can change user',4,'change_user'),(15,'Can delete user',4,'delete_user'),(16,'Can view user',4,'view_user'),(17,'Can add content type',5,'add_contenttype'),(18,'Can change content type',5,'change_contenttype'),(19,'Can delete content type',5,'delete_contenttype'),(20,'Can view content type',5,'view_contenttype'),(21,'Can add session',6,'add_session'),(22,'Can change session',6,'change_session'),(23,'Can delete session',6,'delete_session'),(24,'Can view session',6,'view_session');
+INSERT INTO `auth_permission` VALUES (1,'Can add log entry',1,'add_logentry'),(2,'Can change log entry',1,'change_logentry'),(3,'Can delete log entry',1,'delete_logentry'),(4,'Can view log entry',1,'view_logentry'),(5,'Can add permission',2,'add_permission'),(6,'Can change permission',2,'change_permission'),(7,'Can delete permission',2,'delete_permission'),(8,'Can view permission',2,'view_permission'),(9,'Can add group',3,'add_group'),(10,'Can change group',3,'change_group'),(11,'Can delete group',3,'delete_group'),(12,'Can view group',3,'view_group'),(13,'Can add user',4,'add_user'),(14,'Can change user',4,'change_user'),(15,'Can delete user',4,'delete_user'),(16,'Can view user',4,'view_user'),(17,'Can add content type',5,'add_contenttype'),(18,'Can change content type',5,'change_contenttype'),(19,'Can delete content type',5,'delete_contenttype'),(20,'Can view content type',5,'view_contenttype'),(21,'Can add session',6,'add_session'),(22,'Can change session',6,'change_session'),(23,'Can delete session',6,'delete_session'),(24,'Can view session',6,'view_session'),(25,'Can add booking',7,'add_booking'),(26,'Can change booking',7,'change_booking'),(27,'Can delete booking',7,'delete_booking'),(28,'Can view booking',7,'view_booking'),(29,'Can add menu',8,'add_menu'),(30,'Can change menu',8,'change_menu'),(31,'Can delete menu',8,'delete_menu'),(32,'Can view menu',8,'view_menu');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,7 +233,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -242,7 +242,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(6,'sessions','session');
+INSERT INTO `django_content_type` VALUES (1,'admin','logentry'),(3,'auth','group'),(2,'auth','permission'),(4,'auth','user'),(5,'contenttypes','contenttype'),(7,'restaurant','booking'),(8,'restaurant','menu'),(6,'sessions','session');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -259,7 +259,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -268,7 +268,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2025-03-11 18:20:48.051562'),(2,'auth','0001_initial','2025-03-11 18:20:49.394023'),(3,'admin','0001_initial','2025-03-11 18:20:49.710439'),(4,'admin','0002_logentry_remove_auto_add','2025-03-11 18:20:49.739321'),(5,'admin','0003_logentry_add_action_flag_choices','2025-03-11 18:20:49.757940'),(6,'contenttypes','0002_remove_content_type_name','2025-03-11 18:20:50.019382'),(7,'auth','0002_alter_permission_name_max_length','2025-03-11 18:20:50.193703'),(8,'auth','0003_alter_user_email_max_length','2025-03-11 18:20:50.255750'),(9,'auth','0004_alter_user_username_opts','2025-03-11 18:20:50.272927'),(10,'auth','0005_alter_user_last_login_null','2025-03-11 18:20:50.397615'),(11,'auth','0006_require_contenttypes_0002','2025-03-11 18:20:50.405911'),(12,'auth','0007_alter_validators_add_error_messages','2025-03-11 18:20:50.426508'),(13,'auth','0008_alter_user_username_max_length','2025-03-11 18:20:50.583758'),(14,'auth','0009_alter_user_last_name_max_length','2025-03-11 18:20:50.755947'),(15,'auth','0010_alter_group_name_max_length','2025-03-11 18:20:50.810046'),(16,'auth','0011_update_proxy_permissions','2025-03-11 18:20:50.829736'),(17,'auth','0012_alter_user_first_name_max_length','2025-03-11 18:20:50.970472'),(18,'sessions','0001_initial','2025-03-11 18:20:51.045064');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2025-03-11 18:20:48.051562'),(2,'auth','0001_initial','2025-03-11 18:20:49.394023'),(3,'admin','0001_initial','2025-03-11 18:20:49.710439'),(4,'admin','0002_logentry_remove_auto_add','2025-03-11 18:20:49.739321'),(5,'admin','0003_logentry_add_action_flag_choices','2025-03-11 18:20:49.757940'),(6,'contenttypes','0002_remove_content_type_name','2025-03-11 18:20:50.019382'),(7,'auth','0002_alter_permission_name_max_length','2025-03-11 18:20:50.193703'),(8,'auth','0003_alter_user_email_max_length','2025-03-11 18:20:50.255750'),(9,'auth','0004_alter_user_username_opts','2025-03-11 18:20:50.272927'),(10,'auth','0005_alter_user_last_login_null','2025-03-11 18:20:50.397615'),(11,'auth','0006_require_contenttypes_0002','2025-03-11 18:20:50.405911'),(12,'auth','0007_alter_validators_add_error_messages','2025-03-11 18:20:50.426508'),(13,'auth','0008_alter_user_username_max_length','2025-03-11 18:20:50.583758'),(14,'auth','0009_alter_user_last_name_max_length','2025-03-11 18:20:50.755947'),(15,'auth','0010_alter_group_name_max_length','2025-03-11 18:20:50.810046'),(16,'auth','0011_update_proxy_permissions','2025-03-11 18:20:50.829736'),(17,'auth','0012_alter_user_first_name_max_length','2025-03-11 18:20:50.970472'),(18,'sessions','0001_initial','2025-03-11 18:20:51.045064'),(19,'restaurant','0001_initial','2025-03-12 13:36:25.103742');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -296,6 +296,56 @@ LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant_booking`
+--
+
+DROP TABLE IF EXISTS `restaurant_booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `restaurant_booking` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `guest_id` int NOT NULL,
+  `booking_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant_booking`
+--
+
+LOCK TABLES `restaurant_booking` WRITE;
+/*!40000 ALTER TABLE `restaurant_booking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurant_booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant_menu`
+--
+
+DROP TABLE IF EXISTS `restaurant_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `restaurant_menu` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `inventory` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant_menu`
+--
+
+LOCK TABLES `restaurant_menu` WRITE;
+/*!40000 ALTER TABLE `restaurant_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurant_menu` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -306,4 +356,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-11 18:21:49
+-- Dump completed on 2025-03-12 13:39:46
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Booking, Menu
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "price", "inventory")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "guest_id", "booking_date")
